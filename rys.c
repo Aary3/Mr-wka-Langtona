8d3651ebacf939e184e25ef5965ae9a58f4c02ac
Piotr <piotrsosnowskidev@onet.pl>
2024-01-13 20:51:28 +0100

    Zmieniłem żeby plansza była rysowana do podanego pliku, nie na stdout

--- rys.c
+++ rys.c
@@ -9,23 +9,23 @@ void wypisz(FILE *file, plansza **array, int m, int n) {
 		for (int j = 1; j <= 2*n+1; j++) {		//kolumny
 			y = (j-1)/2;
 			if (i == 1 && j == 1)			//najpierw warunki dla rogów
-				printf("┌");
+				fprintf(file, "┌");
 			else if (i == 1 && j == 2*n+1)
-				printf ("┐");
+				fprintf (file, "┐");
 			else if (i == 2*m+2 && j == 1)
-				printf("└");
+				fprintf(file, "└");
 			else if (i == 2*m+2 && j == 2*n+2)
-				printf("┘");
+				fprintf(file, "┘");
 			else if ( i % 2 == 1)			//jak jesteśmy w nieparzystym wierszu
-				printf("─");			//jak jesteśmy w parzystym wierszu
+				fprintf(file, "─");			//jak jesteśmy w parzystym wierszu
 			else if (j % 2 == 1)			//jak jesteśmy w nieparzystej kolumnie
-				printf("│");
+				fprintf(file, "│");
 			else if (array[x][y].state == 1)
-				printf("█");
+				fprintf(file, "█");
 			else
-				printf(" ");
+				fprintf(file, " ");
 
 		}
-	printf("\n");	
+	fprintf(file, "\n");	
 }
 }
