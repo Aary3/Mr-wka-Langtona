ed7aba3c482970503e94f71350a5779a98f979f8
git <01187521@pw.edu.pl>
2024-01-14 22:23:55 +0100

    przenioslem pliki do src

---
+++ src/plansza.h
@@ -0,0 +1,19 @@
+#ifndef PLANSZA_H
+#define PLANSZA_H
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct komorka{ //idea: zadeklarowac pointer[x][y] o wymiarach planszy
+	bool state; 	// i trzymac stan.
+} plansza;
+
+typedef struct mrow{
+	int kierunek; //0< 1^ 2> 3v
+	int x;
+	int y;
+} mrowisko;
+
+int mrowka_operacja(mrowisko mrowka, plansza **array);
+
+#endif
