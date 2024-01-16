784db928a28875876fe9754025a97568834baed2
Piotr <piotrsosnowskidev@onet.pl>
2024-01-16 20:50:47 +0100

    Dodałem tworzenie nazwy pliku wyjściowego z sprintf

--- src/main.c
+++ src/main.c
@@ -7,5 +7,12 @@ int main(int argc, char **argv) {
 	getarg(argc, argv, arglist);
 	if(arglist.load==0)
 		plansza board[arglist.m][arglist.n];
+	char *file_out = malloc(50*sizeof *file_out);
+	if(arglist.filename == NULL)
+		sprintf(file_out, "plansza_%diteracji", arglist.i);
+	else
+		sprintf(file_out, "%s_%diteracji", arglist.filename, arglist.i);
+	FILE *f = fopen(file_out, "w");
+	
 	return 0;
 }
