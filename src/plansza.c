97b1b0b1608d201b402f98c91696c4e1ef0b913f
Piotr <piotrsosnowskidev@onet.pl>
2024-01-18 15:13:05 +0100

    Poprawiłem, bo w plansza.c było na odwrót mrowka->x i mrowka->y w zmianie state tablicy XD

--- src/plansza.c
+++ src/plansza.c
@@ -1,17 +1,17 @@
 #include "plansza.h"
 
 int mrowka_operacja(mrowisko *mrowka, plansza **array, arg *arglist){
-	switch(array[mrowka->x][mrowka->y].state) 
+	switch(array[mrowka->y][mrowka->x].state) 
 	{
 		case 0:
 			mrowka->kierunek+=1;
 			mrowka->kierunek%=4;//aby nie wyszlo za zakres kierunkow;
-			array[mrowka->x][mrowka->y].state=1;
+			array[mrowka->y][mrowka->x].state=1;
 			break;
 		case 1:
 			mrowka->kierunek+=3;//symuluje +=4 ; -=1(+4 gwarantuje ze nie zejdziemy na liczbe ujemna i daje reszte 0 przy mod4 wiec nie ma znaczenia a -1 to kierunek w lewo)
 			mrowka->kierunek%=4;
-			array[mrowka->x][mrowka->y].state=0;
+			array[mrowka->y][mrowka->x].state=0;
 			break;
 	}
 	switch(mrowka->kierunek)
